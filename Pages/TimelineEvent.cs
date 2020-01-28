d70ad4bfa0866244d0cba91dc36d94e7e76a90a4
Mike <mboss@student.ethz.ch>
2020-01-28 17:13:59 +0100

    Added children

--- Pages/TimelineEvent.cs
+++ Pages/TimelineEvent.cs
@@ -8,4 +8,14 @@ public class TimelineEvent
     public TimelineEvent[] children { get; set; }
 
     public DateTime Date => Convert.ToDateTime(date);
+
+    public TimelineEvent(string date)
+    {
+        this.date = date;
+        this.title = null;
+        this.description = null;
+        this.children = null;
+    }
+
+    public TimelineEvent(){}
 }
